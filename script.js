a57837680f131cf3b6f1bdf23a7f055383fd5829
luigipr <luigiprofeti@gmail.com>
2023-04-14 09:46:12 -0300

    versão com os data-tests colocados

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let userName = '';
 let messages = [];
+let user ='';
+let load;
 axios.defaults.headers.common['Authorization'] = '2GkGoRP9ETlDO5k8paZaQY5V';
 
+chooseName();
+
+function pagestart() {
 const promisse = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
 promisse.then(processMessages)
 
+}
 function processMessages(response){
     console.log(response);
     
@@ -12,22 +18,39 @@ function processMessages(response){
 
     renderMessages();
 }
+// setInterval(pagestart(), 3000);
+// setInterval(online(), 5000);
 
 function chooseName() {
     let promptname = prompt('digite seu nome:');
     const objName = {
         name: promptname
     }
+    user = promptname;
     console.log(objName);
     const promisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', objName);
 
-    promisse.then(success);
+    promisse.then(pagestart());
     promisse.catch(failure);
 
 }
 
 
-chooseName();
+// function logoff() {
+//     load = window.location.reload();
+// }
+
+// function online() {
+//     const stillon = {
+//         name: user
+//     }
+
+//     const promisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', stillon);
+
+//     promisse.then( continueon );
+//     promisse.catch( logoff );
+// }
+
 
 function renderMessages() {
     const ulMessages = document.querySelector('.messages')
@@ -44,12 +67,12 @@ function renderMessages() {
 	// 	time: "08:01:17"
          if (message.type === 'status') {
          ulMessages.innerHTML += `
-             <li class='status-message'>
+             <li class='status-message' data-test="message">
                  <i>(${message.time})</i>  <strong>${message.from}</strong> entra na sala...
              </li>`
             } else {
             ulMessages.innerHTML +=  
-            `<li class='message'>
+            `<li class='message' data-test="message">
                 <i>(${message.time})</i>  <strong>${message.from}</strong> para <strong>${message.to}</strong>: 
             </li>`
         }
@@ -58,59 +81,55 @@ function renderMessages() {
     
 }
 
-function adicionarReceita(){
-
-
+function sendMessage(){
     // pegar os dados que foram digitados pelo usuario nos inputs e textareas
-    const campoTitulo = document.querySelector('.nome-receita');
-    const campoIngredientes = document.querySelector('.ingredientes-receita');
-    const campoPreparo = document.querySelector('.modo-preparo-receita');
+    const messagefield = document.querySelector('.text');
+    const text = messagefield.value
+    const today = new Date()
+    const now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
     // criar um novo objeto com os dados da receita
-    const novaReceita = {
-        titulo: campoTitulo.value,
-        ingredientes: campoIngredientes.value,
-        preparo: campoPreparo.value
+    const newmessage = {
+        from: user,
+	 	to: "Todos",
+	 	text: text,
+	 	type: "message",
+	 	time: now
     };
+    console.log(newmessage)
+  
+    console.log('vai enviar a rmensagem');
 
-    // adicionar uma nova receita no array de receitas
-    // receitas.push( novaReceita );
-
-    // enviar a nova receita para ser salva no servidor
-    // 1 - preciso de uma ferramenta para fazer tudo funcionar => axios!!!!
-
-    // 2 - enviar a nova receita para o servidor e pegar a resposta
-    
-    console.log('vai enviar a receita');
-
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v2/tastecamp/receitas', novaReceita);    
+    const promisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', newmessage);    
     // sucesso!
-    promessa.then( receberResposta ); // agendando a execucao da funcao quando a resposta chegar no meu computador  
-    promessa.catch( deuErro ); // executa uma função se ocorrer algum erro, falha, etc! 
-    console.log('receita foi enviada ao servidor');    
-
-    // axios.get();
-    
-
-    renderizarReceitas();
-
+    promisse.then( renderMessages ); // agendando a execucao da funcao quando a resposta chegar no meu computador  
+    messagefield.value ='';
+    //promisse.catch(error);
+        
 }
 
+function error(response) {
+    console.log(response)
+}
 
 function success(response) {
     console.log(response);
     console.log(response.data);
-    const promisse = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
-    promisse.then( answer );
 }
 function failure(error) {
     console.log('ocorreu um erro')
     chooseName();
 }
 
-function answer(response) {
-    messages = response.data;
-    //renderMessages();
-}
+// function answer(response) {
+//     messages = response.data;
+//     //renderMessages();
+// }
 
-
+const input = document.querySelector(".text");
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    document.querySelector('.plane').click();
+  }
+});
